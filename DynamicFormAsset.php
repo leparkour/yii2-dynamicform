e3fe45c0f76b44d9b6cfad58ac27eff369ba682d
leparkour <leparkour17@gmail.com>
2023-09-15 02:24:32 +0300

    Update DynamicFormAsset.php

--- DynamicFormAsset.php
+++ DynamicFormAsset.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/wbraganca/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace wbraganca\dynamicform;
+namespace leparkour\dynamicform;
 
 /**
  * Asset bundle for dynamicform Widget
